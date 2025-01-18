d5c669fddd07e4bde8dffcf96b82f9c6deb1a557
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2025-01-19 00:34:14 +0700

    array

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 const s string = "constant"
@@ -42,11 +43,43 @@ func loop() {
 		fmt.Println("n modulo", n)
 	}
 }
+func condition() {
+	if 7%2 == 0 {
+		fmt.Println("divisible")
+	} else {
+		fmt.Println("not divisible by 2")
+	}
+	if 8%4 == 0 {
+		fmt.Println("divisible by 4")
+	}
 
+}
+
+func switchCase() {
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Its the weekend")
+	default:
+		fmt.Println("Its weekday")
+	}
+}
+func array() {
+	var arr [5]int
+	fmt.Println(len(arr))
+	//let compiler count the length of array
+	var barr = [...]int{1, 2, 3}
+	fmt.Println(barr)
+	for idx := range len(barr) {
+		fmt.Println(barr[idx])
+	}
+}
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println(os.Getenv("USER"), ", Let's be friends!")
 	fmt.Println(s)
 	testVariables()
 	loop()
+	condition()
+	switchCase()
+	array()
 }
